43a5b2b46b6a5b398a10751fdc7d7aa0b0402f56
MrudhulaThota <thotamrudhula.2006@gmail.com>
2025-09-22 13:56:41 +0530

    update changes

---
+++ app.js
@@ -0,0 +1,2 @@
+var_a=67
+console.log(var_a); 
---
+++ datatype.js
@@ -0,0 +1,28 @@
+
+// data types in js
+        // primitive data types
+        // 1. number, 2. string, 3. boolean, 4. null, 5. undefined
+        // non primitive data types
+        // 1. object, 2. array, 3. function
+        // this and window are commands of js
+        // datatypes in js
+var a=67
+console.log(a);
+var b="hello"  //string
+console.log(b);
+var c=true  //boolean
+console.log(c);
+var d;  //undefined
+console.log(d);
+var e=null  //null  
+console.log(e);
+// non primitive data types
+array=[56,"hello",true,null]
+console.log(typeof array);
+
+obj={
+    name:"Siri",
+    age:12,
+    city:"Hyderabad"
+}
+console.log(typeof obj);
